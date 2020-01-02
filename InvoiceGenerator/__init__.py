77c1edb81e67b5513bdb8e9b35ef3eb07b060f19
mrajeswarasai <mrajeswarasai@gmail.com>
2020-01-02 11:25:22 +0530

    Revert "Added Project FIles"
    This reverts commit 3e7260b82e6a0137afbc0541675901889f549d91.

--- InvoiceGenerator/__init__.py
+++
@@ -1,4 +0,0 @@
-VERSION = (1, 0, 1)
-
-__version__ = VERSION
-__versionstr__ = '.'.join(map(str, VERSION))
--- buyer/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Orders_Buying, Orders_Leasing, Notification, Leasing_Notification
-# Register your models here.
-admin.site.register(Orders_Buying)
-admin.site.register(Orders_Leasing)
-admin.site.register(Notification)
-admin.site.register(Leasing_Notification)
